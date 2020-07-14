655f3ceca1d554779406a66863f60a746250cd84
Vladyslav Levchenko <vladyslav.levchenko@litslink.com>
2020-07-14 16:12:29 +0300

    Done sending contact_form

--- js/contact_us.js
+++ js/contact_us.js
@@ -1,105 +1,48 @@
-const CAPTCHA_KEY = "6Les8KYZAAAAAAmhgeHbwWvDye9k5RYQgEGLnknr";
-const CAPTCHA_VERIFICATION_URL = "https://www.google.com/recaptcha/api/siteverify";
-const FILE_MAX_SIZE = 20;
-const secretKey = "6Les8KYZAAAAAAmhgeHbwWvDye9k5RYQgEGLnknr";
-
-$.ready(() => {
-  contactUs = {
-    toggleContactForm: function () {
-      $(".contact_success").toggle();
-      $(".contact_spinner").hide();
-    },
+$(document).ready(function () {
+  const resetForm = () => {
+    $(".contact_success").toggle();
+    $(".contact_spinner").toggle();
+    grecaptcha.reset();
+    $("#contact_form")[0].reset();
+    $(".fileClear").hide();
+    $(".fileName").hide().text("");
+    $(".fileUploadError").text("");
+    $('.toggle_contact_form button[type="submit"]').disabled = true;
+    $(".countrySelect").val("");
+    $(".countrySelect").trigger("change");
   };
-});
 
-const show = function (elem) {
-  elem.style.display = "block";
-};
-
-const hide = function (elem) {
-  elem.style.display = "none";
-};
-
-const toggle = function (elem) {
-  if (window.getComputedStyle(elem).display === "block") {
-    hide(elem);
-    return;
-  }
-  show(elem);
-};
-
-const formData = new FormData();
-const serializeArray = function (form) {
-  const arr = [];
-  Array.prototype.slice.call(form.elements).forEach(function (field) {
-    if (!field.name || field.disabled || ["reset", "submit", "button"].indexOf(field.type) > -1) {
-      return;
-    }
-    if (field.type === "select-multiple") {
-      Array.prototype.slice.call(field.options).forEach(function (option) {
-        if (!option.selected) return;
-        arr.push({
-          name: field.name,
-          value: option.value,
-        });
+  function sendForm(clientData) {
+    fetch("/sendmail", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify(clientData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          resetForm();
+        } else {
+          toggle(spinerContactUs);
+          alert("Failed captcha verification, try again.");
+          $(".captchaNote").text = "Failed captcha verification.";
+        }
       });
-      return;
-    }
-    if (["checkbox", "radio"].indexOf(field.type) > -1 && !field.checked) return;
-    arr.push({
-      name: field.name.replace(/-/gi, field.name.charAt(0).toUpperCase()),
-      value: field.value,
+  }
+
+  $("#contact_form").on("submit", function (e) {
+    e.preventDefault();
+    $(".contact_spinner").toggle();
+    const contactFormData = $(this).serializeArray();
+    const contactFormObject = {};
+    $.each(contactFormData, function (_, v) {
+      contactFormObject[v.name.replace(/-/gi, v.name.charAt(0).toUpperCase())] = v.value;
     });
-    if (field.name === "file") {
-      // arr.push({ name: field.name, value: field.files[0].name });
-    }
+    contactFormObject.file = $(".fileInput").val().split("\\").reverse()[0];
+    sendForm(contactFormObject);
   });
-  return arr;
-};
 
-const dialogSuccess = document.getElementsByClassName("contact_success")[0];
-const spinerContactUs = document.getElementsByClassName("contact_spinner")[0];
-const captchaNote = document.getElementsByClassName("captchaNote")[0];
-const contactForm = document.getElementById("contact_form");
-const button = document.querySelectorAll('.toggle_contact_form button[type="submit"]');
-
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  toggle(spinerContactUs);
-
-  const formData = {};
-  const formattedData = serializeArray(contactForm);
-  formattedData.forEach((element) => {
-    if (formData[element.name] !== undefined) {
-      if (!formData[element.name].push) {
-        formData[element.name] = [formData[this.name]];
-      }
-      formData[element.name].push(element.value || "");
-    } else {
-      formData[element.name] = element.value || "";
-    }
+  $("#contact_res_btn").on("click", () => {
+    $(".contact_success").toggle();
   });
-  fetch("/sendmail", {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
-    body: JSON.stringify(formData),
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.success) {
-        toggle(dialogSuccess);
-        toggle(spinerContactUs);
-        grecaptcha.reset();
-        contactForm.reset();
-        button[0].disabled = true;
-      } else {
-        captchaNote.innerHTML = "Failed captcha verification";
-      }
-    });
-});
-
-const contactResBtn = document.getElementById("contact_res_btn");
-
-contactResBtn.addEventListener("click", () => {
-  toggle(dialogSuccess);
 });
--- js/fileUpload.js
+++ js/fileUpload.js
@@ -1,25 +1,38 @@
-$(document).ready(function() {
-  $('.fileInput').change(function (e) {
-
-    if(e.target.files[0].size > 10485760) {
-      $('.fileInput').val('');
-      $('.fileName').hide().text('');
-      $('.fileUploadError').text('File size should not exceed 10MB');
-      $('.fileClear').hide();
+$(document).ready(function () {
+  $(".fileInput").change(function (e) {
+    if (e.target.files[0].size > 10485760) {
+      $(".fileInput").val("");
+      $(".fileName").hide().text("");
+      $(".fileUploadError").text("File size should not exceed 10MB");
+      $(".fileClear").hide();
       return false;
-    }
-    else {
-      $('.fileName').show().text(e.target.files[0].name);
-      $('.fileClear').show();
-      $('.fileUploadError').text('');
+    } else {
+      $(".fileName").show().text(e.target.files[0].name);
+      $(".fileClear").show();
+      $(".fileUploadError").text("");
     }
   });
 
-  $('.fileClear').click(function (e) {
+  $(".fileClear").click(function (e) {
     e.preventDefault();
-    var inputGroup = $(this).parents('.fileUpload');
-    inputGroup.find('.fileInput').val('');
-    inputGroup.find('.fileName').show().text('');
+    var inputGroup = $(this).parents(".fileUpload");
+    inputGroup.find(".fileInput").val("");
+    inputGroup.find(".fileName").show().text("");
     $(this).hide();
+  });
+
+  $("#file").on("change", function () {
+    const file = $(this)[0].files[0];
+    fetch("/upload", {
+      method: "POST",
+      headers: { "Content-type": "application/octet-stream" },
+      body: file,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (!data.files.file.path) {
+          console.log("File upload failed.. ", data);
+        }
+      });
   });
 });
